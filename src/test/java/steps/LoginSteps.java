7b25653a13845a6d9bd1cf2ad7c74536879fd327
Pau Leyva García <93128845+Leyva03@users.noreply.github.com>
2025-01-07 01:44:22 +0100

    Last changes

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -14,8 +14,8 @@ public class LoginSteps {
 
     WebDriver driver;
 
-    @Given("I am on the login page")
-    public void iAmOnTheLoginPage() throws InterruptedException {
+    @Given("the user is on the login page")
+    public void TheUserIsOnTheLoginPage() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver-win64/chromedriver.exe");
         driver = new ChromeDriver();
         driver.navigate().to("https://demo.prestashop.com/");
@@ -25,47 +25,47 @@ public class LoginSteps {
         signInLink.click();
     }
 
-    @When("I enter the username {string}")
-    public void iEnterTheUsername(String username) throws InterruptedException {
+    @When("the user enter the username {string}")
+    public void TheUserEnterTheUsername(String username) throws InterruptedException {
         Thread.sleep(3000);
         WebElement emailField = driver.findElement(By.id("field-email"));
         emailField.clear();
         emailField.sendKeys(username);
     }
 
-    @When("I enter the password {string}")
-    public void iEnterThePassword(String password) throws InterruptedException {
+    @When("the user enter the password {string}")
+    public void TheUserEnterThePassword(String password) throws InterruptedException {
         Thread.sleep(3000);
         WebElement passwordField = driver.findElement(By.id("field-password"));
         passwordField.clear();
         passwordField.sendKeys(password);
     }
 
-    @When("I log in")
-    public void iLogIn() throws InterruptedException {
+    @When("the user log in")
+    public void TheUserLogIn() throws InterruptedException {
         Thread.sleep(2000);
         WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
         loginButton.click();
         Thread.sleep(3000);
     }
 
-    @Then("I should be logged in")
-    public void iShouldBeLoggedIn() {
+    @Then("the user should be logged in")
+    public void TheUserShouldBeLoggedIn() {
         WebElement userDashboard = driver.findElement(By.xpath("//a[contains(@href, 'my-account')]"));
         Assert.assertTrue(userDashboard.isDisplayed(), "El usuario no ha iniciado sesión correctamente.");
         driver.quit();
     }
 
-    @Then("I should see the login error message {string}")
-    public void iShouldSeeTheLoginErrorMessage(String errorMessage) {
+    @Then("the user should see the login error message {string}")
+    public void TheUserShouldSeeTheLoginErrorMessage(String errorMessage) {
         WebElement errorAlert = driver.findElement(By.cssSelector(".alert-danger"));
         Assert.assertTrue(errorAlert.isDisplayed(), "No se muestra el mensaje de error.");
         Assert.assertEquals(errorAlert.getText().trim(), errorMessage, "El mensaje de error no coincide.");
         driver.quit();
     }
 
-    @Then("I should not be able to log in and should be reminded by the page to complete the field")
-    public void iShouldNotBeAbleToLogInAndShouldBeRemindedByThePageToCompleteTheField() {
+    @Then("the user should not be able to log in and should be reminded by the page to complete the field")
+    public void TheUserShouldNotBeAbleToLogInAndShouldBeRemindedByThePageToCompleteTheField() {
         WebElement fieldError = driver.findElement(By.cssSelector(".form-control-feedback"));
         Assert.assertTrue(fieldError.isDisplayed(), "No se muestra el recordatorio para completar el campo.");
         driver.quit();
